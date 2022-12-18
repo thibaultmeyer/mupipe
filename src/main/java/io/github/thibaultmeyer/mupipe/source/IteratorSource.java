f450dc25dc619e6d005763974eebc60bdb0275b5
Thibault Meyer <meyer.thibault@gmail.com>
2022-12-18 11:59:13 +0100

    Add IteratorSource
    Signed-off-by: Thibault Meyer <meyer.thibault@gmail.com>

---
+++ src/main/java/io/github/thibaultmeyer/mupipe/source/IteratorSource.java
@@ -0,0 +1,41 @@
+package io.github.thibaultmeyer.mupipe.source;
+
+import java.util.Iterator;
+
+/**
+ * Reads elements from an iterator.
+ *
+ * @param <T> Element type
+ */
+public final class IteratorSource<T> implements Source<T> {
+
+    private final Iterator<T> source;
+
+    /**
+     * Build a new instance.
+     *
+     * @param source Source to use
+     */
+    public IteratorSource(final Iterator<T> source) {
+
+        this.source = source;
+    }
+
+    @Override
+    public boolean hasNext() {
+
+        return this.source.hasNext();
+    }
+
+    @Override
+    public T nextElement() {
+
+        return this.source.next();
+    }
+
+    @Override
+    public void close() {
+
+        // Nothing to do
+    }
+}
---
+++ src/test/java/io/github/thibaultmeyer/mupipe/source/IteratorSourceTest.java
@@ -0,0 +1,66 @@
+package io.github.thibaultmeyer.mupipe.source;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+final class IteratorSourceTest {
+
+    @Test
+    void hasNext() throws Exception {
+
+        // Arrange
+        final Iterator<String> dataList = List.of("apple").iterator();
+        final Source<String> source = new IteratorSource<>(dataList);
+
+        // Act
+        final boolean hasNextFirst = source.hasNext();
+        source.nextElement();
+
+        final boolean hasNextSecond = source.hasNext();
+        source.close();
+
+        // Assert
+        Assertions.assertTrue(hasNextFirst);
+        Assertions.assertFalse(hasNextSecond);
+    }
+
+    @Test
+    void nextElement() throws Exception {
+
+        // Arrange
+        final Iterator<String> dataList = List.of("apple").iterator();
+        final Source<String> source = new IteratorSource<>(dataList);
+
+        // Act
+        final String element = source.nextElement();
+        source.close();
+
+        // Assert
+        Assertions.assertNotNull(element);
+        Assertions.assertEquals("apple", element);
+    }
+
+    @Test
+    void nextElementNoMoreElement() throws Exception {
+
+        // Arrange
+        final Iterator<String> dataList = List.<String>of().iterator();
+        final Source<String> source = new IteratorSource<>(dataList);
+
+        // Act
+        final NoSuchElementException exception = Assertions.assertThrows(
+            NoSuchElementException.class,
+            source::nextElement);
+        source.close();
+
+        // Assert
+        Assertions.assertNotNull(exception);
+    }
+}
